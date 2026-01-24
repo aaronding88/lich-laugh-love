5f2cf40cc0978fc899b35bb13b3c6cae220b8dac
Aaron Ding <aaronding88@gmail.com>
2026-01-23 16:38:29 -0800

    Add rough draft animation tree to Orc unit

--- data/battle_unit/battle_unit.gd
+++ data/battle_unit/battle_unit.gd
@@ -6,6 +6,35 @@ extends Area2D
 @onready var animated_skin: AnimatedSprite2D = $AnimatedSkin
 @onready var health_bar: ProgressBar = $HealthBar
 @onready var fatigue_bar: ProgressBar = $FatigueBar
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $AnimationTree
+
+
+var velocity := Vector2.ZERO
+	
+func _physics_process(_delta: float) -> void:
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed("ui_up"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y -= 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+
+	if velocity == Vector2.ZERO:
+		animation_tree.get("parameters/playback").travel("idle")
+	else:
+		animation_tree.get("parameters/playback").travel("running")
+		animation_tree.set("parameters/idle/blend_position", velocity)
+		animation_tree.set("parameters/running/blend_position", velocity)
+	
+func _input(input: InputEvent) -> void:
+	if input.is_action_pressed("ui_down"):
+		animation_player.play("run_down")
+	if input.is_action_released("ui_down"):
+		animation_player.play("idle")
 
 func set_stats(value: UnitStats) -> void:
 	stats = value
@@ -20,5 +49,4 @@ func set_stats(value: UnitStats) -> void:
 	collision_layer = stats.team + 1
 	
 	animated_skin.sprite_frames = stats.sprite_frames
-	animated_skin.play()
 		 
